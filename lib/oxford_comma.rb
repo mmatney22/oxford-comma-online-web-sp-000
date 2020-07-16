90725847f866a300d2be7bc5c862b23bda8ffb7c
Morgan Matney <morgan.matney89@gmail.com>
2020-07-16 19:48:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
-  if array.length == 2
+  if array.size == 1
+    array[0]
+  elsif array.size == 2
     array.join(" and ")
-  else
-    array[0...-2].join(", ") + ", and " + array[-1]
   end
 end
